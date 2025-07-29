fd38e7fded06cf0b894085d4347fd7f2d81e3cf1
Hritick05 <150815002+Hritick05@users.noreply.github.com>
2025-07-29 22:35:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Add/remove 'scrolled' class based on scroll position
+window.addEventListener('scroll', () => {
+  const navbar = document.getElementById('navbar');
+  if (window.scrollY > 50) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+});
